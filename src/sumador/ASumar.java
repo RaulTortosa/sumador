93191034b2b844df418b1543cef18736f48f03f1
Raul Rodriguez <tortosaraul@gmail.com>
2022-05-25 00:34:38 +0200

    Preparado el metodo para la prueba negativa

--- src/sumador/ASumar.java
+++ src/sumador/ASumar.java
@@ -12,6 +12,9 @@ public class ASumar {
         if (numero.length() == 1) {
             return (numero + " = " + numero);
         }
+        if (numero.contains("-")) {
+        	return "Negativo. Siempre negativo";
+        }
 		return null;
 	}
 
--- test/sumador/TestASumar.java
+++ test/sumador/TestASumar.java
@@ -5,12 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TestASumar {
-
+	
 	@Test
 	public void testUnaCifra() {
 		ASumar sum = new ASumar();
 		String resultado = sum.sumativo("7");
 		assertEquals("7 = 7", resultado);
 	}
+
+	@Test
+	public void testNegativo() {
+		ASumar sum = new ASumar();
+		String resultado = sum.sumativo("-7");
+		assertEquals("7 = 7", resultado);
+	}
+	
 
 }
